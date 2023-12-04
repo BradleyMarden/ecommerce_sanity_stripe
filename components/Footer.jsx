9dba2136d8917312e197f90b0600004e33ebdaf0
Bradley Hanson-Marden <bradleymarden@ez-gamelogic.com>
2023-12-04 12:31:34 +0000

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <p>2023 Slo Rides All rights reserverd - Created by Cyrex Studios.
           <Image
           className="image"
-          src = '/../public/assets/CS_logo.png'
+          src = '/assets/CS_logo.png'
           width={25}
           height={25}
           alt="Picture of the author"/>
