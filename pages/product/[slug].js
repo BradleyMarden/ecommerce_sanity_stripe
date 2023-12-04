61ca833f37327ddb445a1470e5bd665300f61fe2
Bradley Hanson-Marden <bradleymarden@ez-gamelogic.com>
2023-12-04 12:03:42 +0000

    fix screen res

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -71,7 +71,7 @@ const ProductDetails = ({ product, products }) => {
 
       <div className="maylike-products-wrapper">
           <h2>You may also like</h2>
-          <div className="marquee">
+          <div className="marquee11">
             <div className="maylike-products-container track">
               {products.map((item) => (
                 <Product key={item._id} product={item} />
