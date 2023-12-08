a39ad1f624cf792b833fd73bb4974caa340e607d
Bradley Hanson-Marden <bradleymarden@ez-gamelogic.com>
2023-12-08 02:35:20 +0000

    fixed sizing

--- sanity_ecommerce/schemas/category.js
+++ sanity_ecommerce/schemas/category.js
@@ -14,7 +14,14 @@ export default {
             type: 'image',
             options: {
                 hotspot: true,
-            }
+            },
+            validation: Rule => Rule.custom(image => {
+                if (!image) return true
+                const { dimensions } = decodeAssetId(image.asset._ref)
+                if(dimensions.width === 500 && dimensions.height === 750)
+                    return true
+                return "Image doesnt meet requirements of 500w 750h. Current image size: W: " + dimensions.width + " H: "+ dimensions.height
+            })
         },
         {
             name: "slug",
@@ -33,3 +40,15 @@ export default {
         }
     ]
 };
+const pattern = /^image-([a-f\d]+)-(\d+x\d+)-(\w+)$/
+
+const decodeAssetId = id => {
+    const [, assetId, dimensions, format] = pattern.exec(id)
+    const [width, height] = dimensions.split("x").map(v => parseInt(v, 10))
+
+    return {
+        assetId,
+        dimensions: {width, height},
+        format,
+    }
+}
